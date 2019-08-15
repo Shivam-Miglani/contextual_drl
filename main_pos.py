559d87f9ed55f816e987019b82d73d900eeb4e84
Shivam Miglani <shivam@x005097.its-s.tudelft.nl>
2019-08-15 22:10:10 +0200

    good dumps

--- main_pos.py
+++ main_pos.py
@@ -18,7 +18,7 @@ def preset_args():
     parser = argparse.ArgumentParser()
 
     envarg = parser.add_argument_group('Environment')
-    envarg.add_argument("--domain", type=str, default='win2k', help="")
+    envarg.add_argument("--domain", type=str, default='cooking', help="")
     envarg.add_argument("--contextual_embedding", type=str, default='word2vec', help="")
     envarg.add_argument("--model_dim", type=str, default=50, help="embedding dimension")  # word2vec 50.
     envarg.add_argument("--num_words", type=int, default=500, help="number of words to consider for act model is 500. Arg model is 100") # 100 if arguments.
